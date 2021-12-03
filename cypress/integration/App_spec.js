63fe72dc42494456c85f11ca28d3602bfb92974c
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-03 14:47:56 -0700

    install router

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -2,8 +2,9 @@ describe('Main Page User Flows', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   });
-  it('Should see a button', () => {
-    cy.get('button')
+  it('Should see a header', () => {
+    cy.get('.App-link')
+    .should('be.visible')
   });
 
 })
