b7b1ba5bf1a906af17bbf4b96bf42eac3e755a46
Nadia Naji <nadialnaji@gmail.com>
2021-12-02 22:55:09 -0700

    Remove App.js unit test

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,16 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import StartVillage from '../StartVillage/StartVillage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
         <Route exact path="/" render={() =>
           <StartVillage />
         }
         />
+      </Switch>
     </div>
   );
 }
