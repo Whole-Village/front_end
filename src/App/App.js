f0c1272dc69a9c6d812be8f41319f9df663c9553
Nadia Naji <nadialnaji@gmail.com>
2021-12-02 22:53:01 -0700

    Remove Switch from App.js to troubleshoot CircleCI

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import StartVillage from '../StartVillage/StartVillage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
         <Route exact path="/" render={() =>
           <StartVillage />
         }
         />
-      </Switch>
     </div>
   );
 }
