b5f81d4bfc82744065a3f9f480077de6334941f2
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-04 16:40:57 -0700

    Add some styling to form

--- src/NewEvent/NewEvent.js
+++ src/NewEvent/NewEvent.js
@@ -8,22 +8,26 @@ const NewEvent = () => {
     setValue({...value, [event.target.name]: event.target.value});
   };
   return (
-    <form>
-      <label htmlFor="name">Event Name:</label>
-      <input type="text" name="name" onChange={onChange} />
-      <label htmlFor="date">Event Date:</label>
-      <input type="date" name="date" onChange={onChange} />
-      <label htmlFor="time">Event Time:</label>
-      <input type="time" name="time" onChange={onChange} />
-      <label htmlFor="description">Event Description:</label>
-      <input type="text" name="description" onChange={onChange} />
-      <label>Is an adult required?</label>
-      <label  htmlFor="adultRequired">Yes</label>
-      <input type="radio" name="adult" onChange={onChange} value={true} />
-      <label  htmlFor="adultRequired">No</label>
-      <input type="radio" name="adult" onChange={onChange} value={false} />
-      <input type="submit" />
-    </form>
+    <div className="form-container">
+      <form className="form">
+          <label htmlFor="name">Event Name:</label>
+          <input type="text" style={{width:150}} name="name" onChange={onChange} />
+          <label htmlFor="date">Event Date:</label>
+          <input type="date" style={{width:150}} name="date" onChange={onChange} />
+          <label htmlFor="time">Event Time:</label>
+          <input type="time" style={{width:150}} name="time" onChange={onChange} />
+          <label htmlFor="description">Event Description:</label>
+          <input type="text" style={{height:100, width:300}} name="description" onChange={onChange} />
+          <label>Is an adult required?</label>
+          <div>
+            <label  htmlFor="adultRequired">Yes</label>
+            <input type="radio" name="adult" onChange={onChange} value={true} />
+            <label  htmlFor="adultRequired">No</label>
+            <input type="radio" name="adult" onChange={onChange} value={false} />
+          </div>
+          <input type="submit" />
+      </form>
+    </div>
   )
 }
 
