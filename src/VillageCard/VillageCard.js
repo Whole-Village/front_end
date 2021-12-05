f2cec6329f5806af69e783194544fc693c068efa
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-05 15:35:18 -0700

    Style village cards

--- src/VillageCard/VillageCard.js
+++ src/VillageCard/VillageCard.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './VillageCard.css'
 import './VillageCard.js'
 
 const VillageCard = ({ id, name }) => {
   return (
+    <>
   <Link to={`/villages/${id}`}>
-    <article className="village-card">
-      <h3>{name}</h3>
-    </article>
+    <div className="village-card">
+  <div className="color-indicator" />
+      <div className="event-card-info">
+      <p>{name}</p>
+</div>
+    </div>
   </Link>
+    <Link to={`/villages/${id}`}>
+    <div className="village-card">
+      <p>{name}</p>
+    </div>
+  </Link>
+    <Link to={`/villages/${id}`}>
+    <div className="village-card">
+      <p>{name}</p>
+    </div>
+  </Link>
+  </>
   )
 }
 
