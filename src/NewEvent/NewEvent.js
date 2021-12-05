ee4c0d1642600d5dfe06c11536e4c6e2e1e31bd4
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-04 22:40:30 -0700

    Style new event form to comp

--- src/NewEvent/NewEvent.js
+++ src/NewEvent/NewEvent.js
@@ -10,22 +10,20 @@ const NewEvent = () => {
   return (
     <div className="form-container">
       <form className="form">
-          <label htmlFor="name">Event Name:</label>
-          <input type="text" style={{width:150}} name="name" onChange={onChange} />
-          <label htmlFor="date">Event Date:</label>
-          <input type="date" style={{width:150}} name="date" onChange={onChange} />
-          <label htmlFor="time">Event Time:</label>
-          <input type="time" style={{width:150}} name="time" onChange={onChange} />
-          <label htmlFor="description">Event Description:</label>
-          <input type="text" style={{height:100, width:300}} name="description" onChange={onChange} />
-          <label>Is an adult required?</label>
-          <div>
-            <label  htmlFor="adultRequired">Yes</label>
-            <input type="radio" name="adult" onChange={onChange} value={true} />
-            <label  htmlFor="adultRequired">No</label>
-            <input type="radio" name="adult" onChange={onChange} value={false} />
-          </div>
-          <input type="submit" />
+        <label htmlFor="name" className="name-label">NAME</label>
+        <input type="text" style={{width:300}} name="name" className="name-input" onChange={onChange}/>
+        <label htmlFor="date" className="date-label">DATE</label>
+        <input type="date" style={{width:300}} name="date" className="date-input" onChange={onChange}/>
+        <label htmlFor="time" className="time-label">TIME</label>
+        <input type="time" style={{width:300}} name="time" className="time-input" onChange={onChange}/>
+        <label className="adult-label">Is an adult required?</label>
+        <label  htmlFor="adultRequired" className="yes-label">Yes</label>
+        <input type="radio" name="adult" className="yes-input" onChange={onChange} value={true}/>
+        <label  htmlFor="adultRequired" className="no-label">No</label>
+        <input type="radio" name="adult" className="no-input" onChange={onChange} value={false}/>
+        <label htmlFor="description" className="description-label">DESCRIPTION</label>
+        <input type="text" style={{height:100, width:300}} name="description" className="description-input" onChange={onChange} />
+        <input type="submit" className="submit-btn" style={{width:300}} />
       </form>
     </div>
   )
