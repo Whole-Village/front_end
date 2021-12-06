1efe468ba597257d40736d29af42155ecbb4b400
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-06 14:26:41 -0700

    Add new event form pop up functionality

--- src/VillageHome/VillageHome.js
+++ src/VillageHome/VillageHome.js
@@ -3,22 +3,36 @@ import React from 'react';
 import './VillageHome.css'
 import NavVillageHome from '../NavVillageHome/NavVillageHome';
 import Events from '../Events/Events';
+import NewEvent from '../NewEvent/NewEvent';
 import { villages } from '../Fixtures/Villages';
+import { useState, useEffect } from 'react';
 
 const VillageHome = ({ id }) => {
-
+  const [isFormOpen, setFormStatus] = useState(false)
   const villageData = villages.filter((elem) => {
     return parseInt(elem.id) === 1})
+
+  const showEventForm = () => {
+    return (
+      setFormStatus(true)
+    )
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    setFormStatus(false)
+  }
 
   return (
     <div className="village-home">
       <NavVillageHome />
       <h2>Welcome to {villageData[0].name}!</h2>
       <div className="sub">
+      {isFormOpen && <NewEvent />}
         <div className="events">
           <p>Upcoming Events</p>
-          <button>Create a New Event</button>
-          <Events />
+          <button onClick={showEventForm}>Create a New Event</button>
+          <Events sumitForm={submitForm}/>
         </div>
         <div className="villagers">
         </div>
