48cf1761bbb7050357ca8341ab7c414e263a8bc3
Nadia Naji <nadialnaji@gmail.com>
2021-12-05 17:20:08 -0700

    Change cypress test file to check for header on main page

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -2,8 +2,8 @@ describe('Main Page User Flows', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   });
-  it('Should see a button', () => {
-    cy.get('button')
+  it('Should show header', () => {
+    cy.get('header')
   });
 
 })
