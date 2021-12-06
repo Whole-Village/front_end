52189ec95dc73028cd987287924c1afcbfbf9697
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-05 21:12:41 -0700

    Change to path

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,9 +1,11 @@
 describe('Main Page User Flows', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000', { timeout: 120000 })
+    cy.visit('http://localhost:3000')
   });
   it('Should show header', () => {
     cy.get('header')
   });
 
 })
+
+    // cy.visit('http://localhost:3000', { timeout: 120000 })
