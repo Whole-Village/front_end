bbbf99c9911f32a3a593057dd90c7b8692043b80
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-06 09:33:48 -0700

    Try adding s/ to end of url

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,6 +1,6 @@
 describe('Main Page User Flows', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
   });
   it('Should see an input', () => {
     cy.get('input')
