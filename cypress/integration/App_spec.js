ea8e32e0f799db4bcaea3ae79096273f68c9371d
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-05 21:08:26 -0700

    Wait for server

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,6 +1,6 @@
 describe('Main Page User Flows', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000', { timeout: 120000 })
   });
   it('Should show header', () => {
     cy.get('header')
