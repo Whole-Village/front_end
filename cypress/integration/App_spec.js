74c81c4b3d163405ad9ebbfbca0c83de3a4df83f
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-06 08:31:33 -0700

    Change cypress test

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,11 +1,12 @@
-// describe('Main Page User Flows', () => {
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000')
-  // });
-  // it('Should see a button', () => {
-  //   cy.get('button')
-  // });
+describe('Main Page User Flows', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/dashboard')
+  });
+  it('Should see a button', () => {
+    cy.get('h1')
+    .should('be.visible')
+  });
 
-// })
+})
 
     // cy.visit('http://localhost:3000', { timeout: 120000 })
