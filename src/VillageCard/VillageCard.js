1a4acfb3bdd6e3bd4c40283ada6afc8ce3983d43
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-06 18:03:59 -0700

    Display Village icons

--- src/VillageCard/VillageCard.js
+++ src/VillageCard/VillageCard.js
@@ -1,30 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './VillageCard.css'
 import './VillageCard.js'
 
 const VillageCard = ({ id, name }) => {
   return (
     <>
-  <Link to={`/villages/${id}`}>
-    <div className="village-card">
-      <div className="color-indicator" />
-      <div className="event-card-info">
-        <p>{name}</p>
-      </div>
-    </div>
-  </Link>
-    <Link to={`/villages/${id}`}>
-    <div className="village-card">
-      <p>{name}</p>
-    </div>
-  </Link>
-    <Link to={`/villages/${id}`}>
-    <div className="village-card">
-      <p>{name}</p>
-    </div>
-  </Link>
-  </>
+    <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+    <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+        <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+        <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+        <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+        <NavLink className="navlink__village-card" to={`/villages/${id}`}>
+      <section>
+        <p>Soccer Team</p>
+      </section>
+    </NavLink>
+    </>
   )
 }
 
