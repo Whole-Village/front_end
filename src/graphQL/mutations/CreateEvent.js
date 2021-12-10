871ae6aefde628f8b8f4cf9034f58c3e2620e91c
Anna Johnson <ae.johnson2931@gmail.com>
2021-12-10 15:40:25 -0700

    Create Add Event mutation file

---
+++ src/graphQL/mutations/CreateEvent.js
@@ -0,0 +1,25 @@
+import { gql, useMutation } from '@apollo/client';
+
+export const createEvent = gql`
+mutation createEvent($villageId: ID!, $name: String!, $description: String!, $date: String, $time: String, $adultRequired: Boolean){
+    createEvent(input: {
+     villageId: $villageId,
+     name: $name,
+     description: $description,
+     date: $date,
+     time: $time,
+     adultRequired: $adultRequired
+   }) {
+     event {
+       id
+       name
+       description
+       date
+       time
+       adultRequired
+       villageName
+     }
+     errors
+   }
+  }
+`
