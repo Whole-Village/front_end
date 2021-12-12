54ab6b35a46622be0e38a15810ed9ede25e7628d
Nadia Naji <nadialnaji@gmail.com>
2021-12-12 13:49:10 -0700

    Remove mutation import for updateUser in dashboard

--- src/graphQL/mutations/CreateVillage.js
+++ src/graphQL/mutations/CreateVillage.js
@@ -1,23 +1,23 @@
 import { gql } from '@apollo/client';
 
 export const createVillage = gql`
-mutation createVillage($name: String!, $description: String, $members: [String!]){
-    createVillage(
-     name: $name,
-     description: $description,
-     userEmails: $members
-    ) {
-     village {
-       id
-       name
-       description
-       users {
-           firstName
-           lastName
-           email
-         }
-       }
-     }
-     errors
-  }
+    mutation createVillage($name: String!, $description: String, $userEmails: [String!]){
+      createVillage(
+      name: $name,
+      description: $description,
+      userEmails: $userEmails
+      ) {
+      village {
+        id
+        name
+        description
+        users {
+          firstName
+          lastName
+          email
+        }
+      }
+      errors
+      }
+    }
 `
