7963fc64cd5f2f9bec0efb3960dcc9bc7a122f84
Nadia Naji <nadialnaji@gmail.com>
2021-12-14 16:19:15 -0700

    Restructure NewVillagersAdded

--- src/AddNewVillagerForm/AddNewVillagerForm.js
+++ src/AddNewVillagerForm/AddNewVillagerForm.js
@@ -10,20 +10,21 @@ const AddNewVillagerForm = ({ villageId, setNewVillagerFormOpen }) => {
   const [sendInvitations, {data, loading}] = useMutation(createVillageMember);
   const [noInviteeError, setNoInviteeError] = useState(false)
   const [serverError, setServerError] = useState('')
-  // const [noInviteeError, setNoInviteeError] = useState({[member]:d.createVillageMember.errors[0]})
-  const addVillagers = () => {
-    if(!noInviteeError) {
+
+  console.log(newVillager.email)
+  const addVillagers = (e) => {
+    e.preventDefault()
+    if(!newVillager.email){
+      setNoInviteeError(true)
+    } else {
       sendInvitations({
         variables: {
-          userEmail: newVillageMembers[0],
+          userEmail: newVillager,
           villageId: parseInt(villageId)
         },
         onCompleted: d => populateServerError(d.createVillageMember.errors[0])
       })
     }
-    // setNewVillagerFormOpen(false)
-    setNoInviteeError(false)
-    // window.location.reload()
   }
 
   const populateServerError = (error) => {
@@ -33,24 +34,21 @@ const AddNewVillagerForm = ({ villageId, setNewVillagerFormOpen }) => {
     return
   }
 
-  const handleNewVillagers = (e) => {
-    // e.preventDefault();
-    if(newVillageMembers.length === 0) {
-      setNewVillageMembers([...newVillageMembers, newVillager.email])
-    }
-    if(!newVillager.email) {
-      setNoInviteeError(true)
-    }
-  }
+  // const handleNewVillagers = (e) => {
+  //   console.log(newVillageMembers[0])
+  //   e.preventDefault();
+  //   if(!newVillageMembers[0]) {
+  //     setNewVillageMembers([...newVillageMembers, newVillager.email])
+  //     setNoInviteeError(true)
+  //   }
+  // }
 
   const handleInviteNewVillager = (e) => {
     e.preventDefault()
     addNewVillager({[e.target.name]: e.target.value})
       console.log(newVillager)
   }
-
-  console.log(data)
-  console.log(data?.createVillageMember.errors)
+  console.log(newVillager)
 
   return(
     <div className='new-villager-form-modal'>
@@ -63,21 +61,15 @@ const AddNewVillagerForm = ({ villageId, setNewVillagerFormOpen }) => {
             value={newVillager.email}
             onChange={e => handleInviteNewVillager(e)}
           />
-        <button className='add-member-btn' onClick={(e) => handleNewVillagers(e)}>
-          <span className="material-icons">
-            person_add
-          </span>
-        </button>
         </label>
-        <button className='send-invite' onClick={addVillagers}>Invite!</button>
+        <button className='send-invite' onClick={e => addVillagers(e)}>Invite!</button>
       </div>
       <div className='invitees'>
         <h2 className='village-headers'>Village Invite List</h2>
         <div className='roster'>
           <NewVillagersAdded
           noInviteeError={noInviteeError}
-          newVillageMembers={newVillageMembers}
-          setNewVillageMembers={setNewVillageMembers}
+          newVillager={newVillager}
           serverError={serverError}
           />
         </div>
--- src/NewVillagersAdded/NewVillagersAdded.js
+++ src/NewVillagersAdded/NewVillagersAdded.js
@@ -2,39 +2,45 @@ import React from 'react';
 
 
 
-const NewVillagersAdded = ({ newVillageMembers, setNewVillageMembers, serverError }) => {
+const NewVillagersAdded = ({ newVillager, serverError, noInviteeError }) => {
 
-  const removeVillageMember = (villagerEmail) => {
-    console.log(newVillageMembers)
-    const updatedVillageList = newVillageMembers.filter(villager => villager !== villagerEmail)
-    setNewVillageMembers(updatedVillageList)
-    console.log(newVillageMembers)
-  }
+  // const removeVillageMember = (villagerEmail) => {
+  //   console.log(newVillageMembers)
+  //   const updatedVillageList = newVillageMembers.filter(villager => villager !== villagerEmail)
+  //   setNewVillageMembers(updatedVillageList)
+  //   console.log(newVillageMembers)
+  // }
 
-  const newInvitees = newVillageMembers.map(member => {
-    return(
-      <div className='village-member-card' key={Math.random()}>
-        <div className="added-villager-container">
-          {serverError && <p>You've entered an invalid email address. Try again, please!</p>}
-          {member ?
-            <>
-            <div className='village-member-email'>{member}</div>
-              <button className='remove-member-btn' onClick={() => removeVillageMember(member)}>
-                <span className='material-icons'>
-                  person_remove_alt_1
-                </span>
-              </button>
-            </> :
-            <p>You haven't entered any new invitees. Try again, please!</p>
-          }
-        </div>
-      </div>
-    )
-  })
+  // const checkError = () => {
+  //   if(serverError) {
+  //     return <p>You've entered an invalid email address. Try again, please!</p>
+  //   } else if(inviteeError) {
+  //     return <p>You've entered an invalid email address. Try again, please!</p>
+  //   } else {
+  //     const newInvitees = newVillageMembers.map(member => {
+  //       <div className='village-member-card' key={Math.random()}>
+  //       <div className="added-villager-container">
+  //           <div className='village-member-email'>{member}</div>
+  //             <button className='remove-member-btn' onClick={() => removeVillageMember(member)}>
+  //               <span className='material-icons'>
+  //                 person_remove_alt_1
+  //               </span>
+  //             </button>
+  //             </div>
+  //       </div>
+  //     })
+  //     return newInvitees;
+  //   }
+  // }
+
 
   return(
     <div className='added-villagers'>
-      {newInvitees}
+      <div className='village-member-card' key={Math.random()}>
+        <div className="added-villager-container">
+            <div className='village-member-email'>{newVillager}</div>
+        </div>
+      </div>
     </div>
 
   )
